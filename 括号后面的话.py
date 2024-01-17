80a6e0834a59d3511d9df3672e5db3b85644d046
AoikamiMio <89596549+AoikamiMio@users.noreply.github.com>
2024-01-17 11:33:30 +0100

    Add files via upload

---
+++ 括号后面的话.py
@@ -0,0 +1,26 @@
+def text_input():
+    text = input("内容：")
+    a = None
+    b = None
+    c = None
+
+    if "（" in text or "(" in text:
+        start = text.find("（") if "（" in text else text.find("(")
+        b = text[start+1:]
+        c = text[:start]
+
+    else:
+        c = text
+
+    if b and ("）" in b or ")" in b):
+        end = b.find("）") if "）" in b else b.find(")")
+        b = b[:end]
+
+    if b is None or b == "" or b == "不是" or b == "bushi":
+        a = "不是"
+
+    else:
+        a = b
+        
+    print(c,"（",a,"）")
+text_input()
